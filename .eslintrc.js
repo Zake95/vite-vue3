e8e8ef604bc7593cb901315ba75263badc1199ad
zhangk <1466940130@qq.com>
2022-03-11 18:05:01 +0800

    代码提交eslint检查测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,15 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {}
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+    'import/no-unresolved': 'off'
+  }
 }
